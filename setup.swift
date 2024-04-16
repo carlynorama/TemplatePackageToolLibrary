afc2e5bbe6135f54f16657829cc74529ddaa9020
carlynorama <carlynorama@gmail.com>
2024-04-16 15:43:14 -0700

    fix forgotten file names

--- setup.swift
+++ setup.swift
@@ -1,7 +1,7 @@
 #!/usr/bin/env swift
 
-    //    chmod +x setup.swift
-    //    ./setup.swift clone clean
+//    chmod +x setup.swift
+//    ./setup.swift clone clean
 
 
 import Foundation
@@ -20,14 +20,17 @@ print("Script:", script)
 /// you can get the input arguments by dropping the first element
 var inputArgs = CommandLine.arguments.dropFirst()
 
+//------------------------------------------------------------------------------
+//MARK: Name & Target Dir
 if inputArgs.contains("-f") {
     newPrefix = utilities.enclosingFolder
+    target = "tmp"
 } else {
     print("Please enter new package name:")
-        guard let tmpPrefix = readLine(strippingNewline: true) else {
-            fatalError("Can't continue without a name.")
-        }
-        newPrefix = tmpPrefix
+    guard let tmpPrefix = readLine(strippingNewline: true) else {
+        fatalError("Can't continue without a name.")
+    }
+    newPrefix = tmpPrefix
     if target.isEmpty {
         print("Create subfolder with this name? (alternative is to use this directory)")
         if let confirm = readLine(strippingNewline: true) {
@@ -42,10 +45,13 @@ if inputArgs.contains("-f") {
             target = "tmp"
         }
     }
-    
+}
+if target.isEmpty {
+    fatalError("what did I miss?")
 }
 
-
+//------------------------------------------------------------------------------
+//MARK: What am I doing?
 if inputArgs.contains("all") {
     inputArgs = ["clone", "gitclean", "rename", "setupclean", "init"]
 }
@@ -54,11 +60,6 @@ if inputArgs.contains("post") {
     inputArgs = ["gitclean", "rename", "setupclean", "init"]
 }
 
-
-
-if target.isEmpty {
-    target = "tmp"
-}
 //------------------------------------------------------------------------------
 //MARK: clone
 if inputArgs.contains("clone") {
@@ -70,15 +71,16 @@ if inputArgs.contains("clone") {
         fatalError("Template not downloadable due to \(error)")
     }
 }
+
 //------------------------------------------------------------------------------
 //MARK: gitclean
-if inputArgs.contains("gitclean") { 
+if inputArgs.contains("gitclean") {
     do {
         try utilities.removeExtraGitIgnore(in: target)
     } catch {
         fatalError("gitignore update failed due to: \(error)")
     }
-
+    
     do {
         try utilities.deleteGitFolder(in: target)
     } catch {
@@ -92,7 +94,7 @@ if inputArgs.contains("rename") {
     print("\(newPrefix) in \(target)")
     
     do {
-        try utilities.replaceMyTool(with: newPrefix, in: target)
+        try utilities.replace("MyTool", with: newPrefix, in: target)
     } catch {
         fatalError("name not full replaced due to \(error)")
     }
@@ -115,74 +117,80 @@ if target == "tmp" {
         try utilities.moveToCurrentDirectory(from: target)
     } catch {
         fatalError("problem moving files due to \(error)")
-    }   
+    }
 }
 
 //------------------------------------------------------------------------------
-//MARK: init 
+//MARK: init
 if inputArgs.contains("init") {
     let repoDir =  target == "tmp" ? "." : target
     do {
         try utilities.initializeRepo(at: repoDir)
     } catch {
         fatalError("couldn't init due to \(error)")
-    }  
+    }
 }
 
 //--------------------------------------------------------------------------------------------------
 //--------------------------------------------------------------------------------------------------
 //MARK: UtilityHandler
 struct UtilityHandler {
-
+    
     let shell:String
     let path:String
     let pwd:String
-
+    
     let gitURL:URL
-
+    
     var enclosingFolder:String {
         let index = pwd.lastIndex(of: "/")!
         return String(pwd.suffix(from: pwd.index(after: index)))
     }
     
-
+    
     public func clone(_ repo: String, to destination: String = "") throws {
         //try UtilityHandler.runPublicProcess(UtilityHandler.git_long, arguments: ["clone", repo, destination])
-        try UtilityHandler.runPublicProcess(gitURL, 
-                                            arguments: ["clone", repo, destination], 
+        try UtilityHandler.runPublicProcess(gitURL,
+                                            arguments: ["clone", repo, destination],
                                             environment: ["PATH":path, "SHELL":shell]
-                                            )
+        )
     }
-
+    
     public func deleteGitFolder(in target: String) throws {
         try UtilityHandler.privateShell("rm -rf \(target)/.git", as: shell)
     }
-
+    
     public func deleteSetupFiles(in target: String) throws {
         try UtilityHandler.privateShell("rm -f \(target)/SETUP.md", as: shell)
         try UtilityHandler.privateShell("rm -f \(target)/setup.swift", as: shell)
     }
-
+    
     public func removeExtraGitIgnore(in target: String) throws {
         //FileManager.default.fileExists(atPath: "\(target)/.gitignore")
         let url = URL(fileURLWithPath: "\(target)/.gitignore")
         try UtilityHandler.trimFrom("#", in: url)
     }
-
-    public func replaceMyTool(with newName:String, in target:String) throws {
+    
+    public func replace(_ phrase:String, with newPhrase:String, in target:String) throws {
         let url = URL(fileURLWithPath: "\(target)")
         let files = try UtilityHandler.enumerateFiles(in: url)
         //print(files)
         for file in files {
-            try UtilityHandler.replaceAllOccurrences(of:"MyTool", in:file, with: newName)
+            try UtilityHandler.replaceAllOccurrences(of:phrase, in:file, with: newPhrase)
+        }
+        
+        let directories = try UtilityHandler.enumerateDirectories(in: url, pathContains:phrase)
+        for directory in directories {
+            let newDirName = directory.lastPathComponent.replacingOccurrences(of: phrase, with: newPhrase)
+            try UtilityHandler.rename(at: directory, to: newDirName)
         }
     }
-
+    
     public func moveToCurrentDirectory(from target:String) throws {
         let result = try UtilityHandler.privateShell("mv \(target)/{.,}* .; rm -rf \(target)", as: shell)
         print(result)
     }
-
+    
     public func initializeRepo(at repo:String) throws {
         do {
             if repo == "." {
@@ -196,14 +204,14 @@ struct UtilityHandler {
             fatalError("repo did not init correctly")
         }
     }
-
     
-//--------------------------------------------------------------------------------------------------
-//MARK: UtilityHandler - static
-
-    //not currently used in init script. 
+    
+    //--------------------------------------------------------------------------------------------------
+    //MARK: UtilityHandler - static
+    
+    //not currently used in init script.
     private static let git_guess = URL(fileURLWithPath: "/usr/bin/git")
-
+    
     public static func replaceAllOccurrences(of toReplace:String, in url:URL, with replacement:String) throws {
         guard url.isFileURL else {
             print("\(url) is not a file.")
@@ -213,7 +221,7 @@ struct UtilityHandler {
         let newFile = string.replacingOccurrences(of: toReplace, with: replacement)
         try newFile.write(to: url, atomically: true, encoding: .utf8)
     }
-
+    
     public static func trimFrom(_ char:String.Element, in url:URL) throws {
         let string = try String(contentsOf: url)
         guard let index = string.lastIndex(of: char) else{
@@ -223,12 +231,12 @@ struct UtilityHandler {
         let newFile = string.prefix(upTo: index)
         try newFile.write(to: url, atomically: true, encoding: .utf8)
     }
-
+    
     public static func enumerateFiles(in url:URL) throws -> [URL] {
-        let fM = FileManager()
-        guard let enumerator = fM.enumerator(at: url, 
-                                            includingPropertiesForKeys: [.isDirectoryKey], 
-                                            options: [.skipsHiddenFiles])
+        let fM = FileManager.default
+        guard let enumerator = fM.enumerator(at: url,
+                                             includingPropertiesForKeys: [.isDirectoryKey],
+                                             options: [.skipsHiddenFiles])
         else {
             //TODO: throw
             fatalError("no files")
@@ -237,24 +245,65 @@ struct UtilityHandler {
         for case let fileURL as URL in enumerator {
             guard let resourceValues = try? fileURL.resourceValues(forKeys: [.isDirectoryKey]),
                   let isDirectory = resourceValues.isDirectory else {
-                    //TODO: throw
-                    fatalError("no resource value")
-                }
+                //TODO: throw
+                fatalError("no resource value")
+            }
             guard !isDirectory else {
                 continue
             }
             
             files.append(fileURL)
         }
-
+        
         return files
-
+        
+    }
+    
+    public static func enumerateDirectories(in url:URL, pathContains:String? = nil) throws -> [URL] {
+        let fM = FileManager.default
+        guard let enumerator = fM.enumerator(at: url,
+                                             includingPropertiesForKeys: [.isDirectoryKey],
+                                             options: [.skipsHiddenFiles])
+        else {
+            //TODO: throw
+            fatalError("no files")
+        }
+        var folders:[URL] = []
+        for case let dirURL as URL in enumerator {
+            guard let resourceValues = try? dirURL.resourceValues(forKeys: [.isDirectoryKey]),
+                  let isDirectory = resourceValues.isDirectory else {
+                //TODO: throw
+                fatalError("no resource value")
+            }
+            guard isDirectory else {
+                continue
+            }
+            if let pathContains {
+                if dirURL.absoluteString.contains(pathContains)  {
+                    folders.append(dirURL)
+                }
+            } else {
+                folders.append(dirURL)
+            }
+        }
+        
+        return folders
+        
     }
 
-    public static func runPublicProcess(_ url:URL, 
-                                        arguments:[String] = [], 
+    public static func rename(at srcURL:URL, to newName:String) throws {
+        let fM = FileManager.default
+        var tmp = srcURL.deletingLastPathComponent()
+        tmp.append(component: newName)
+        try fM.moveItem(at: srcURL, to: tmp)
+    }
+    
+//--------------------------------------------------------------------------------------------------
+//MARK: Process and Shell 
+    public static func runPublicProcess(_ url:URL,
+                                        arguments:[String] = [],
                                         environment:Dictionary<String,String> = [:]
-                                        ) throws { 
+    ) throws {
         let process = Process()
         process.arguments = arguments
         process.executableURL = url
@@ -264,15 +313,15 @@ struct UtilityHandler {
         try process.run()
         process.waitUntilExit()
     }
-
+    
     @discardableResult
-    public static func runPrivateProcess(_ url:URL, 
-                                        arguments:[String] = [], 
-                                        environment:Dictionary<String,String> = [:]
-                                        ) throws -> String {
+    public static func runPrivateProcess(_ url:URL,
+                                         arguments:[String] = [],
+                                         environment:Dictionary<String,String> = [:]
+    ) throws -> String {
         let process = Process()
         let pipe = Pipe()
-
+        
         if !environment.isEmpty {
             process.environment = environment
         }
@@ -280,7 +329,7 @@ struct UtilityHandler {
         process.standardOutput = pipe
         process.standardError = pipe
         process.arguments = arguments
-
+        
         process.standardInput = nil
         process.executableURL = url
         try process.run()
@@ -289,20 +338,20 @@ struct UtilityHandler {
         let output = String(data: data, encoding: .utf8)!
         
         process.waitUntilExit()
-
+        
         if process.terminationStatus == 0 || process.terminationStatus == 2 {
-          return output
+            return output
         } else {
-          print(output)
-          throw CommandError.unknownError(exitCode: process.terminationStatus)
+            print(output)
+            throw CommandError.unknownError(exitCode: process.terminationStatus)
         }
     }
-
+    
     @discardableResult
     public static func  privateShell(_ command: String, as shellPath:String, environment:Dictionary<String,String> = [:]) throws -> String {
         let process = Process()
         let pipe = Pipe()
-
+        
         if !environment.isEmpty {
             process.environment = environment
         }
@@ -320,25 +369,25 @@ struct UtilityHandler {
         let output = String(data: data, encoding: .utf8)!
         
         process.waitUntilExit()
-
+        
         if process.terminationStatus == 0 || process.terminationStatus == 2 {
-          return output
+            return output
         } else {
-          print(output)
-          throw CommandError.unknownError(exitCode: process.terminationStatus)
+            print(output)
+            throw CommandError.unknownError(exitCode: process.terminationStatus)
         }
     }
-
+    
     enum CommandError: Error {
         case unknownError(exitCode: Int32)
     }
-//--------------------------------------------------------------------------------------------------
+    //--------------------------------------------------------------------------------------------------
 }
 
 //--------------------------------------------------------------------------------------------------
 //MARK: UtilityHandler - Environment Reading Init
 extension UtilityHandler {
-        init?() {
+    init?() {
         let info = ProcessInfo.processInfo
         let environment = info.environment
         //print(environment)
@@ -357,5 +406,3 @@ extension UtilityHandler {
         self.gitURL =  URL(fileURLWithPath: gitPath.trimmingCharacters(in: .whitespacesAndNewlines))
     }
 }
-
-
